96916bca6c4bdf936cf423bcb1e54e1c1b1ca9f5
hayrapetyansami <samihayrapetyan@gmail.com>
2022-06-30 23:14:02 +0400

    film app day 2

--- js/filmapp.js
+++ js/filmapp.js
@@ -1,18 +1,45 @@
 "use strict";
 
-const numberOfFilms = +prompt("Քանի՞ ֆիլմ եք դիտել այսօր", "");
+const numberOfFilms = start();
 
 const personalMovieDB = {
-	count: numberOfFilms >= 1 ? numberOfFilms : 0,
+	count: numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false
+	privat: true
 };
 
-if (numberOfFilms == null || numberOfFilms === 0 || numberOfFilms < 0 || numberOfFilms === "" || typeof(numberOfFilms) === "string"){
-	console.log("Տեղի է ունեցել խնդիր, խնդրում ենք գրել միայն թվեր");
-} else {
+detectUserPersonalLevel();
+rememberMyFilms ();
+yourFavoriteGenres();
+showMyDB();
+
+
+function start (numberOfFilms) {
+	numberOfFilms = parseInt(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", ""));
+	
+	while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms) || numberOfFilms < -1) {
+		numberOfFilms = parseInt(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", ""));
+	}
+
+	return numberOfFilms;
+}
+function rememberMyFilms () {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել", "");
+		const b = prompt("Ինչքա՞ն այդ ֆիլմը կգնահատեիք", "");
+	
+		if (!isNaN(b) && a != null && b != null && a.trim() != "" && b.trim() != "" && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log("DONE");
+		} else {
+			console.log("ERROR");
+			i--;
+		}
+	}
+}
+function detectUserPersonalLevel () {
 	if (numberOfFilms < 10) {
 		console.log("Դուք նայել եք բավականին քիչ ֆիլմեր");
 	} else if (numberOfFilms < 30) {
@@ -23,20 +50,21 @@ if (numberOfFilms == null || numberOfFilms === 0 || numberOfFilms < 0 || numberO
 		console.log("Տեղի է ունեցել խնդիր, ըստ երևույթի դուք թիվ չեք նշել");
 	}
 }
+function yourFavoriteGenres () {
+	for (let i = 0; i <= 2; i++) {
+		const genres = prompt(`Ձեր նախընտրելի ժանրը ${i + 1}`);
 
-for (let i = 0; i < 2; i++) {
-	const a = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել", "");
-	const b = prompt("Ինչքա՞ն այդ ֆիլմը կգնահատեիք");
-
-	if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-		a.trim();
-		b.trim();
-		personalMovieDB.movies[a] = b;
-		console.log("DONE");
-	} else {
-		console.log("ERROR");
-		i--;
+		if (genres != null && genres.trim() != "" && genres.length <= 20) {
+			personalMovieDB.genres[i] = genres
+		} else {
+			i--;
+		}
 	}
 }
-
-console.log(personalMovieDB);
+function showMyDB () {
+	if (personalMovieDB.privat) {
+		console.log(personalMovieDB);
+	} else {
+		console.log("Մեր տվյալների բազան փակ է");
+	}
+}
