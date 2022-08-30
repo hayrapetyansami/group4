0dac6ac8c4f138b4aee966e42b7147458a90a2af
hayrapetyansami <samihayrapetyan@gmail.com>
2022-08-31 00:07:14 +0400

    clock with many bugs

--- date/practice/js/app.js
+++ date/practice/js/app.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const elem = document.createElement("div");
+document.body.append(elem);
+
+// < 10
+setInterval(() => {
+	const date = new Date();
+	elem.innerHTML = `
+		<span>${date.getHours()}</span> :
+		<span>${date.getMinutes()}</span> :
+		<span>${date.getSeconds()}</span>
+		<!--<span>${date.getMilliseconds()}</span>-->
+	`;
+}, 1000);
