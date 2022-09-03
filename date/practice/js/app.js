983426e46e583018ac1d0c045782c11a679c7ec9
hayrapetyansami <samihayrapetyan@gmail.com>
2022-09-03 21:48:24 +0400

    we finished count down app first version and added readme file, where You can find guid, version 1.0.1

--- date/practice/js/app.js
+++ date/practice/js/app.js
@@ -1,15 +1,56 @@
 "use strict";
 
-const elem = document.createElement("div");
-document.body.append(elem);
+function countDownLogic (endTime) {
+	const total = Date.parse(endTime) - Date.parse(new Date());
+	const days = Math.floor( total / (1000 * 60 * 60 * 24) );
+	const seconds = Math.floor( (total / 1000) % 60 );
+	const minutes = Math.floor( (total / 1000 / 60) % 60 );
+	const hours = Math.floor( (total / (1000 * 60 * 60)) % 24);
 
-// < 10
-setInterval(() => {
-	const date = new Date();
-	elem.innerHTML = `
-		<span>${date.getHours()}</span> :
-		<span>${date.getMinutes()}</span> :
-		<span>${date.getSeconds()}</span>
-		<!--<span>${date.getMilliseconds()}</span>-->
+	return {
+		"total" : total,
+		"days" : days,
+		"seconds" : seconds,
+		"minutes" : minutes,
+		"hours" : hours
+	}
+}
+
+function countDownIsNeedZero (n) {
+	return n >= 0 && n < 10 ? `0${n}` : n;
+}
+
+function countDownCreateHTML (elementType, day, hour, minute, second) {
+	const parent = document.querySelector("#count-down-app");
+	parent.innerHTML = `
+		<${elementType}>${day < 0 ? "00 " + " : " : day + " : "}</${elementType}>
+		<${elementType}>${hour < 0 ? "00 " + " : " : hour + " : "}</${elementType}>
+		<${elementType}>${minute < 0 ? "00 " + " : " : minute + " : "}</${elementType}>
+		<${elementType}>${second < 0 ? "00" : second}</${elementType}>
 	`;
-}, 1000);
+
+	return elementType;
+}
+
+function countDownUpdate (elementType, endTime) {
+	const timer = setInterval(() => {
+		const {total, days, hours, minutes, seconds} = countDownLogic(endTime);
+		countDownCreateHTML(
+			elementType,
+			countDownIsNeedZero(days),
+			countDownIsNeedZero(hours),
+			countDownIsNeedZero(minutes),
+			countDownIsNeedZero(seconds)
+		);
+
+		if (total <= 0 ) {
+			clearInterval(timer);
+		}
+	}, 1000);
+}
+
+function countDownAppInit (elementType, endTime) {
+	countDownUpdate (elementType, endTime);
+}
+
+countDownAppInit("span", "2022-09-20");
---
+++ date/practice/readme.md
@@ -0,0 +1,10 @@
+# README
+
+# STEP 1
+on Your HTML file You need to create some HTML element with default important id, like this - id="count-down-app"
+
+# STEP 2
+You need to set finish date in app.js, like this format - "2022-09-20"
+
+# STEP 3
+You need to set HTML tagName on "elementType" parameter, for example "span"
