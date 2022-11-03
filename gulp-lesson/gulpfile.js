353ea38117c6db15f4f7b92d413fd3c76f166deb
hayrapetyansami <samihayrapetyan@gmail.com>
2022-11-04 00:18:54 +0400

    GULP 4, created gulpfile.js, used gulp plugins and babel

---
+++ gulp-lesson/gulpfile.js
@@ -0,0 +1,145 @@
+import gulp from "gulp";
+import concat from "gulp-concat";
+import autoPrefixer from "gulp-autoprefixer";
+import GulpCleanCss from "gulp-clean-css";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import GulpUglify from "gulp-uglify";
+import { deleteSync } from "del";
+import browserSync from "browser-sync";
+import imagemin from "gulp-imagemin";
+import gcmq from "gulp-group-css-media-queries";
+import sourcemaps from "gulp-sourcemaps";
+import babel from "gulp-babel";
+import htmlmin from "gulp-htmlmin";
+
+// fix sass bug
+const sass = gulpSass(dartSass);
+
+// output path
+const path = "./build";
+
+// task for scss convert to css
+async function preproc () {
+	return gulp.src("./src/scss/styles.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(gcmq())
+		.pipe(sourcemaps.init())
+		.pipe(concat("styles.css"))
+		.pipe(autoPrefixer({
+			overrideBrowserslist: ["> 0.0001%"],
+			cascade: false
+		}))
+		.pipe(GulpCleanCss({
+			level: 2
+		}))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest(path+"/css"))
+		.pipe(browserSync.stream());
+}
+
+// task for the other css files to concat in one css file
+async function libsCSS () {
+	return gulp.src("./src/libsCSS/*.css")
+		.pipe(gcmq())
+		.pipe(concat("libs-css-file.css"))
+		.pipe(autoPrefixer({
+			overrideBrowserslist: ["> 0.0001%"],
+			cascade: false
+		}))
+		.pipe(GulpCleanCss({
+			level: 2
+		}))
+		.pipe(gulp.dest(path+"/css"))
+		.pipe(browserSync.stream());
+}
+
+// task for all javascript files with babel
+async function scripts () {
+	return gulp.src("./src/js/*.js")
+		.pipe(sourcemaps.init())
+		.pipe(concat("all.js"))
+		.pipe(babel({
+			presets: ["@babel/preset-env"]
+		}))
+		.pipe(GulpUglify({
+			toplevel: true,
+			warnings: false
+		}))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest(path+"/js"))
+		.pipe(browserSync.stream());
+}
+
+// task for pictures with imagemin
+async function pictures () {
+	return gulp.src("./src/img/**/*")
+		.pipe(imagemin())
+		.pipe(gulp.dest(path+"/img"));
+}
+
+// task for remove build folder and him files
+async function toClear () {
+	return deleteSync([path]);
+}
+
+// task for fonts
+async function fonts () {
+	return gulp.dest("./src/fonts/*")
+		.pipe(gulp.dest(path+"/fonts"));
+}
+
+// task for other js files (libs)
+async function libsJS () {
+	return gulp.src("./src/libsJS/*.js")
+		.pipe(gulp.dest(path+"/libsJS"))
+		.pipe(browserSync.stream());
+}
+
+async function htmls () {
+	return gulp.src("./src/*.html")
+		.pipe(htmlmin({
+			collapseWhitespace: true,
+			removeTagWhitespace: true
+		}))
+		.pipe(gulp.dest(path))
+		.pipe(browserSync.stream());
+}
+
+async function watch () {
+	browserSync.init({
+		server: {
+			baseDir: path
+		},
+		tunnel: false,
+		port: 3000 // default: 3000
+	});
+
+	gulp.watch("./src/scss/**/*.scss", preproc);
+	gulp.watch("./src/libsCSS/*.css", libsCSS);
+	gulp.watch("./src/js/*.js", scripts);
+	gulp.watch("./src/img/**/*", pictures);
+	gulp.watch("./src/fonts/*", fonts);
+	gulp.watch("./src/libsJS/*.js", libsJS);
+	gulp.watch("./src/*.html", htmls);
+	gulp.watch("./*html").on("change", browserSync.reload);
+}
+
+//             run       script
+// gulp.task("toClear", toClear);
+// gulp.task("htmls", htmls);
+// gulp.task("preproc", preproc);
+// gulp.task("libsCSS", libsCSS);
+// gulp.task("scripts", scripts);
+// gulp.task("pictures", pictures);
+// gulp.task("fonts", fonts);
+// gulp.task("libsJS", libsJS);
+
+// task for stream our project
+gulp.task("watch", watch);
+
+// task for build (production) our project
+gulp.task("build", gulp.series(toClear, gulp.parallel(htmls, preproc, libsCSS, scripts, pictures, fonts, libsJS)));
+
+// task for parallel build and watch tasks running
+gulp.task("dev", gulp.series("build", "watch"));
---
+++ gulp-lesson/src/js/index.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+	const user = {
+		name: "Alex",
+		age: 42,
+		city: "NY"
+	};
+
+	console.log(user);
+});
